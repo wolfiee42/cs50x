a79ddc1d09b838620373626c2031c3f3e5bf09a1
wolfiee42 <saifalislam2022@gmail.com>
2024-11-13 21:19:58 +0600

    learning how get_int works and how scanf works under the hood.

---
+++ week04/scanf.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    char *s = malloc(4);
+    if (s == NULL)
+    {
+        return 1;
+    }
+    printf("s: ");
+    scanf("%s: ", s);
+
+    printf("s: %s\n", s);
+
+    free(s);
+    return 0;
+}
